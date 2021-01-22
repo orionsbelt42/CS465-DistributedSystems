e6575f698dbc3a318d7ea17c9f22711883333548
Anton Freeman <asfreeman12@gmail.com>
2021-01-22 10:41:24 -0700

    added char from client var

---
+++ EchoThread.java
@@ -0,0 +1,71 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+class EchoThread implements Runnable
+{
+   private Socket socket;
+   private int count;
+   // private long id;
+   InputStream fromClient;
+   OutputStream toClient;
+   // private PrintWriter printWrite;
+
+   EchoThread(Socket newSocket, int newCount)
+   {
+       socket = newSocket;
+       count = newCount;
+   }
+
+   @Override
+   public void run()
+   {
+       try
+       {
+           // System.out.println(count);
+           fromClient = socket.getInputStream();
+           toClient = socket.getOutputStream();
+       }
+       catch (IOException ioExec) {System.err.println(ioExec);}
+       try {
+           int charInt;
+           String buffer = "";
+           char charFromClient = (char)charInt;
+
+           while (true){
+
+               charInt = fromClient.read();
+
+               if ((char)(charInt) == '\n'){
+
+                   toClient.write('\n');
+               }
+
+               //if ((charInt >= Character.getNumericValue('a') && charInt <= Character.getNumericValue('z')) || (charInt >= Character.getNumericValue('A') && charInt <= Character.getNumericValue('Z')) ){
+
+               else if ((charInt >= (int)'a' && charInt <= (int)'z') || (charInt >= (int)'A' && charInt <= (int)'Z') ) {
+                    toClient.write(charInt);
+               }
+               else
+               {
+                   buffer += (char)charInt;
+               }
+
+           }
+
+           /**
+           //printWrite = new PrintWriter(toClient, true);
+           while(scan.hasNextLine())
+           {
+               String line = scan.nextLine();
+               System.out.println("Client " + count + ": " + line);
+               printWrite.println(line);
+           }
+           **/
+       }
+       catch (IOException ioExec) {System.err.println(ioExec);}
+       finally {
+           System.out.println("Client " + count + " left the session.");
+       }
+   }
+}
