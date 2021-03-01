322d6067444e85c10335eb5462974f8eb57e4bfb
Jordan Colebank <jordancolebank0@gmail.com>
2021-02-28 21:01:52 -0800

    still needs work

--- test/ClientThread.java
+++ test/ClientThread.java
@@ -5,39 +5,41 @@ import java.net.*;
 public class ClientThread implements Runnable {
 
     //Socket socket;
-
-
     String hostName;
     int portNumber;
     String message;
 
 
-    ClientThread(String msg) {
-        hostName = "127.0.0.1";
-        portNumber = 8080;
-        message = msg;
+    // create new client thread
+    ClientThread(String host, int port, String msg) {
+        this.hostName = host;
+        this.portNumber = port;
+        this.message = msg;
     }
 
-    @Override
+    @Override   
     public void run()
     {
 
         try (
+            // create new socket
             Socket echoSocket = new Socket(hostName, portNumber);
             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader( new InputStreamReader(echoSocket.getInputStream()) );
-        ) {
-            //System.out.println("Setup connection");
-            out.println(message);
-            System.out.println("sent: " + message);
+        ) { 
+            // output the message
+            out.write(message);
+
+            // close the socket
             echoSocket.close();
+
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
-            // System.exit(1);
+  
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " +
                 hostName + " for " + message);
-            // System.exit(1);
+
         }
     }
 
