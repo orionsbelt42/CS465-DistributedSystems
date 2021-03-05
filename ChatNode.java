02ee7e51bfe8ef389350bca4c4ada8d9b504e2ed
slf279 <slf279@nau.edu>
2021-03-05 11:30:00 -0700

    migrate P2P into one folder

--- ChatNode.java
+++
@@ -1,8 +0,0 @@
-public class ChatNode{
-    public static NodeInfo ME = null;
-    public static NodeInfo ParticipantList = null;
-
-    public Receiver receiver = null;
-    public Sender sender = null;
-}
-
