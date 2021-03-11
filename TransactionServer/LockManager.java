532ab0747302bc2b09437bd35d6d08ba556c35a0
orionsbelt42 <60899526+orionsbelt42@users.noreply.github.com>
2021-03-11 12:58:22 -0700

    Create LockManager.java

---
+++ TransactionServer/LockManager.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.locks;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockManager {
+    // Recieves lock request from AccountManager
+
+    // Recieves unlock requests from TransactionManager
+}
