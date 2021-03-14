2b50bebb7ed051fdb52ad4e96506bbbedde79dac
slf279 <slf279@nau.edu>
2021-03-13 19:18:51 -0700

    created basic Transaction class, migrating previous TransactionManager capabilities there

--- TransactionServer/TransactionManager.java
+++ TransactionServer/TransactionManager.java
@@ -6,25 +6,4 @@ public class TransactionManager
 		return accounts;
 	}
 
-	public int write( int accountNumber, Transaction transaction )
-	{
-		Account account = getAccount( accountNumber );
-		( TransactionServer.lockManager ).lock( account, transaction, WRITE_LOCK );
-		account.setBalance( balance );
-		return balance;
-	}
-
-	public int read( int accountNumber, Transaction transaction )
-	{
-		Account account = getAccount( accountNumber );
-		( TransactionServer.lockManager ).lock( account, transaction, READ_LOCK );
-		return account.getBalance();
-	}
-
-	public void openTranscation(account1, account2, money)
-	{
-		print(" Opening tansaction...")
-		Account account = getAccount( account1 );
-		Account account = getAccount( account2 );
-	}
 }
