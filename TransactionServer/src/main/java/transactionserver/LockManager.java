d422f5d5312a8d747c06ec5381fcbf2636fb7863
orionsbelt42 <60899526+orionsbelt42@users.noreply.github.com>
2021-03-29 00:02:20 -0700

    Update LockManager.java

--- TransactionServer/src/main/java/transactionserver/LockManager.java
+++ TransactionServer/src/main/java/transactionserver/LockManager.java
@@ -1,53 +1,36 @@
 package transactionserver;
 
-import java.util.*;
+        import java.util.*;
 
-// this one still needs a lot of work
 public class LockManager
 {
-    private Hashtable theLocks;
+    private Hashtable theLocks = new Hashtable();
 
-    public void setLock(Account object, Transaction transaction, LockType lockType)
+    public void setLock(Account account, Transaction transaction, LockType lockType)
     {
-        Transaction elem;
-        ArrayList<Lock> locks;
 
         Lock foundLock;
         synchronized (this)
         {
-            Enumeration enm = theLocks.elements();
-
-            while(enm.hasMoreElements()){
-                elem = enm.nextElement();
-                
-                // WHY DOES THE TRANSACTION CLASS HOLD THE LOCKS WHEN WE ARE CHECKING IF SPECIFIC ACCOUNTS
-                // ARE LOCKED........ OUR SETUP NOW PREVENTS INDIVIDUAL ACCOUNTS FROM HOLDING LOCKS
-
-                // WE MAY NEED TO REFACTOR, OR ELSE CHANGE HOW WE VIEW HOW LOCKS WORK - IF A TRANSACTION LOCKS
-                // ALL ACCOUNTS IT MANAGES WITH THE SAME SINGULAR LOCK IT HOLDS......
-
-                locks = elem.getLockList()
-
-            }
-
-            // find lock associated with this object
-            // if there isn't one, create it and add to hashtable
-        }
-
-        foundLock.acquire(transaction, lockType);
-    }
-    
-    // synchronize this one because we want to remove all entries
-    public synchronized void unLock(Transaction transaction)
-    {
-        Enumeration enm = theLocks.elements();
-        while( enm.hasMoreElements() )
-        {
-            Lock aLock = (Lock) (e.nextElement());
-            if( /*trans is a holder of this lock*/)
+            if (!theLocks.contains(account))
             {
-                aLock.release(transaction);
+                Lock lock = new Lock();
+                theLocks.put(account, lock);
             }
+
+            foundLock = theLocks.get(account);
+
+
         }
+
+        foundLock.acquire(transaction.getTID(), lockType);
+    }
+
+    // synchronize this one because we want to remove all entries
+    public synchronized void unLock(Account account, Transaction transaction)
+    {
+        Lock foundLock = theLocks.get(account);
+        foundLock.release(transaction.getTID());
+
     }
 }
