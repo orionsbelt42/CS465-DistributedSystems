8adc20267d8c45cc7474e9fee070cbbfba29ef97
orionsbelt42 <60899526+orionsbelt42@users.noreply.github.com>
2021-03-28 23:59:53 -0700

    Update Lock.java

--- TransactionServer/src/main/java/transactionserver/Lock.java
+++ TransactionServer/src/main/java/transactionserver/Lock.java
@@ -2,52 +2,87 @@ package transactionserver;
 
 import java.util.*;
 
-public class Lock 
+public class Lock
 {
-    private Account object;
+    //private Account object;
     // transactions that have lock
     private Vector holders;
-    private LockType lockType;
+    private LockType lockedType;
 
-    public Lock( Account acct ){
-        object = acct;
-    }
-    
-    
-    public Account getObject() {
-        return object;
-    }
-
-    public synchronized void acquire( Transaction trans, LockType aLockType){
-        while(/*another transaction holds lock in conflicting mode*/) {
-            try{
-                wait();
-            } catch( InterruptedException except) {
-            }
-        }
-
-        if( holders.isEmpty() ) // no TIDs hold lock
-        { 
-            holders.addElement(trans);
-            lockType = aLockType;
-        } 
-        else if ( /*another transaction holds the lock, share it*/)
-        {
-            if(/*This transaction not a holder*/)
-            {
-                holders.addElement(trans);
-            }
-        }
-        else if (/*this transaction is a holder but needs more exclusive lock*/)
-        {
-            lockType.promote();	
-        }
-    }
-
-    public synchronized void release(Transaction trans)
+    //public Lock( Account acct ){
+    public Lock()
     {
-        holders.removeElement(trans);
-        //set lockType to none
+        holders = new Vector();
+    }
+
+
+//    public Account getObject() {
+ //       return object;
+  //  }
+
+    public synchronized void acquire( int transid, LockType lockType){
+        if (lockType == READ_LOCK)
+        {
+            if ((lockedType == READ_LOCK) || (lockedType == EMPTY_LOCK))
+            {
+                holders.addElement(transid);
+                lockedType = READ_LOCK;
+            }
+        }
+        else if (lockType == WRITE_LOCK)
+        {
+            if (lockedType == EMPTY_LOCK)
+            {
+                holders.addElement(transid);
+                lockedType = WRITE_LOCK;
+            }
+            else
+            {
+                // Wait for other locks to clear
+                while (!holders.isEmpty())
+                {
+                    try{
+                        wait();
+                    } catch( InterruptedException except) {
+                    }
+                }
+                holders.addElement(transid);
+                lockedType = WRITE_LOCK;
+            }
+        }
+
+        //while(/*another transaction holds lock in conflicting mode*/) {
+        //    try{
+        //        wait();
+        //    } catch( InterruptedException except) {
+        //    }
+        //}
+
+        //if( holders.isEmpty() ) // no TIDs hold lock
+        //{
+        //    holders.addElement(trans);
+        //    lockType = lockType;
+        //}
+        //else if ( /*another transaction holds the lock, share it*/)
+        //{
+        //    if(/*This transaction not a holder*/)
+        //    {
+        //        holders.addElement(trans);
+        //    }
+        //}
+        //else if (/*this transaction is a holder but needs more exclusive lock*/)
+        //{
+        //    lockType.promote();
+        //}
+    }
+
+    public synchronized void release(int transid)
+    {
+        holders.removeElement(transid);
+        if (holders.isEmpty())
+        {
+            lockedType = EMPTY_LOCK;
+        }
         notifyAll();
     }
 }
